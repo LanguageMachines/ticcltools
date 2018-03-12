d26a192ab69f18f6ec62b4f7295706367edaeb59
Ko van der Sloot <K.vanderSloot@let.ru.nl>
2018-03-12 14:14:37 +0100

    added a test for chaining

---
+++ tests/testchain.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+bindir=/home/sloot/usr/local/bin
+
+if [ ! -d $bindir ]
+then
+   bindir=/exp/sloot/usr/local/bin
+   if [ !-e $bindir ]
+   then
+       echo "cannot find executables "
+       exit
+   fi
+fi
+
+outdir=OUT
+refdir=OUTreference/BOOK
+datadir=DATA
+
+echo start TICLL-stuff
+
+sort -t'#' -s -k4 -gr $refdir/rank.sorted > $outdir/chaintest.ranked
+echo "start TICLL-chain"
+
+$bindir/TICCL-chain -v $outdir/chaintest.ranked
+
+if [ $? -ne 0 ]
+then
+    echo failed after TICLL-chain
+    exit
+fi
+
+echo "checking chain results...."
+
+diff $outdir/chaintest.ranked.chained $refdir/rank.chained >& /dev/null
+if [ $? -ne 0 ]
+then
+    echo "differences in TICLL-rank results"
+    echo "using diff $outdir/chaintest.ranked.chained $refdir/rank.chained"
+    exit
+fi
+
+diff $outdir/chaintest.ranked.chained.debug $refdir/rank.chained.debug >& /dev/null
+if [ $? -ne 0 ]
+then
+    echo "differences in TICLL-rank results"
+    echo "using diff $outdir/chaintest.ranked.chained.debug $refdir/rank.chained.debug"
+    exit
+else
+    echo OK!
+fi
