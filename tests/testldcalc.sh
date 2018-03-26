eda8ef3386feba581f8cc8dc54de7167c930c76f
Ko van der Sloot <K.vanderSloot@let.ru.nl>
2018-03-26 17:24:31 +0200

    added a new test for ldcalc

---
+++ tests/testldcalc.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+if [ "$1" != "" ]
+then
+    outsub=$1
+else
+    outsub=ldcalctest
+fi
+
+bindir=/home/sloot/usr/local/bin
+
+if [ ! -d $bindir ]
+then
+   bindir=/exp/sloot/usr/local/bin
+   if [ ! -d $bindir ]
+   then
+       echo "cannot find executables "
+       exit
+   fi
+fi
+
+outdir=OUT/$outsub
+outdir=TESTRESULTS
+refdir=OUTreference/BOOK
+testdir=TESTDATA
+datadir=DATA
+
+echo start TICLL-ldcalc
+
+$bindir/TICCL-LDcalc --index $refdir/id.indexNT --hash $refdir/anahash --clean $refdir/clean --LD 2 -t max --artifrq 100000000 -o $outdir/my.ldcalc
+
+if [ $? -ne 0 ]
+then
+    echo "failed in TICCL-LDcalc"
+    exit
+fi
+echo "checking LDcalc results...."
+
+sort $outdir/my.ldcalc  > /tmp/ldcalc
+
+diff /tmp/ldcalc $refdir/ldcalc > /dev/null 2>&1
+
+if [ $? -ne 0 ]
+then
+    echo "differences in Ticcl-ldcalc results"
+    echo "using: diff /tmp/ldcalc $refdir/ldcalc"
+    exit
+fi
+
+echo "OK"
