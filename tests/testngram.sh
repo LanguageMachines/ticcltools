bf31e53fee3643c038cc8b15e83cb83a3d0531b4
Ko van der Sloot <K.vanderSloot@let.ru.nl>
2018-04-25 17:39:52 +0200

    added test

---
+++ tests/testngram.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+bindir=/home/sloot/usr/local/bin
+
+if [ ! -d $bindir ]
+then
+   bindir=/exp/sloot/usr/local/bin
+   if [ ! -d $bindir ]
+   then
+       echo "cannot find executables "
+       exit
+   fi
+fi
+
+outdir=TESTRESULTS
+refdir=OUTreference
+testdir=TESTDATA
+datadir=TRI
+
+echo start TICLL-ldcalc
+
+$bindir/TICCL-LDcalc --index $datadir/mre.indexNT --hash $datadir/mre.anahash --clean $datadir/mre.clean --LD 2 -t max --artifrq 100000000 -o $outdir/ngram.ldcalc
+
+if [ $? -ne 0 ]
+then
+    echo "failed in TICCL-LDcalc"
+    exit
+fi
+echo "checking LDcalc results...."
+
+sort $outdir/ngram.ldcalc  > /tmp/ldcalc
+
+diff /tmp/ldcalc $refdir/ngram.ldcalc > /dev/null 2>&1
+
+if [ $? -ne 0 ]
+then
+    echo "differences in Ticcl-ldcalc results"
+    echo "using: diff /tmp/ldcalc $refdir/ngram.ldcalc"
+    exit
+fi
+
+sort $outdir/ngram.ldcalc.ambi  > /tmp/ldcalc.ambi
+diff /tmp/ldcalc.ambi $refdir/ngram.ldcalc.ambi > /dev/null 2>&1
+
+if [ $? -ne 0 ]
+then
+    echo "differences in Ticcl-ldcalc results"
+    echo "using: diff /tmp/ldcalc.ambi $refdir/ngram.ldcalc.ambi"
+    exit
+fi
+
+echo "OK"
