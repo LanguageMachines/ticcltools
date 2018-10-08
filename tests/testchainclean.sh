050d1fbc45d336061ecca5cca03d51a73ae78d29
Ko van der Sloot <K.vanderSloot@let.ru.nl>
2018-10-08 14:36:02 +0200

    preliminary testresults for chanin cleaning

---
+++ tests/testchainclean.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+bindir=/home/sloot/usr/local/bin
+
+if [ ! -d $bindir ]
+then
+   bindir=/exp/sloot/usr/local/bin
+   if [ ! -d $bindir ]
+   then
+       echo "cannot find executables "
+       exit
+   fi
+fi
+
+outdir=OUT
+refdir=OUTreference/BOOK
+testdata=TESTDATA
+
+
+echo "start TICLL-chainclean"
+
+$bindir/TICCL-chainclean --lexicon $testdata/chain.clean -o /tmp/chained.clean $testdata/test.chained
+
+if [ $? -ne 0 ]
+then
+    echo failed INSIDE TICLL-chainclean
+    exit
+fi
+
+
+echo "checking chain results...."
+diff /tmp/chained.clean $refdir/chained.clean >& /dev/null
+if [ $? -ne 0 ]
+then
+    echo "differences in TICLL-chainclean results"
+    echo "using diff /tmp/chained.clean $refdir/chained.clean"
+    exit
+fi
