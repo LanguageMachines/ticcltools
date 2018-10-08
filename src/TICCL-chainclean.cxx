a4b9db800134363b68766dfa3045d435623837ca
Ko van der Sloot <K.vanderSloot@let.ru.nl>
2018-10-08 10:02:59 +0200

    new program added (not working yet)

---
+++ src/TICCL-chainclean.cxx
@@ -0,0 +1,133 @@
+/*
+  Copyright (c) 2006 - 2018
+  CLST  - Radboud University
+  ILK   - Tilburg University
+
+  This file is part of ticcltools
+
+  ticcltools is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  ticcltools is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+  For questions and suggestions, see:
+      https://github.com/LanguageMachines/ticcltools/issues
+  or send mail to:
+      lamasoftware (at ) science.ru.nl
+
+*/
+#include <unistd.h>
+#include <set>
+#include <map>
+#include <functional>
+#include <vector>
+#include <cstdlib>
+#include <string>
+#include <stdexcept>
+#include <iostream>
+#include <fstream>
+#include <cassert>
+#include <cstring>
+#include <cmath>
+#include "config.h"
+#ifdef HAVE_OPENMP
+#include "omp.h"
+#endif
+#include "ticcutils/StringOps.h"
+#include "ticcutils/CommandLine.h"
+#include "ticcutils/PrettyPrint.h"
+#include "ticcutils/Unicode.h"
+#include "ticcl/unicode.h"
+#include "ticcl/word2vec.h"
+
+using namespace std;
+typedef signed long int bitType;
+using TiCC::operator<<;
+
+void usage( const string& name ){
+  cerr << "usage: " << name << "[options] chainfile " << endl;
+  cerr << "\t\t The chainfiles is an outputfile from TICCL-chain." << endl;
+  cerr << "-t--lexicon A validated lexicon." << endl;
+  cerr << "-t--artifrq The artifreq. Default 100000000." << endl;
+  cerr << "\t-o <outputfile> name of the outputfile." << endl;
+  cerr << "\t-h or --help this message." << endl;
+  cerr << "\t-v be verbose, repeat to be more verbose. " << endl;
+  cerr << "\t-V or --version show version. " << endl;
+  exit( EXIT_FAILURE );
+}
+
+int main( int argc, char **argv ){
+  TiCC::CL_Options opts;
+  try {
+    opts.set_short_options( "vVho:" );
+    opts.set_long_options( "lexicon:,artifreq:" );
+    opts.init( argc, argv );
+  }
+  catch( TiCC::OptionError& e ){
+    cerr << e.what() << endl;
+    usage( argv[0] );
+    exit( EXIT_FAILURE );
+  }
+  string progname = opts.prog_name();
+  if ( argc < 2	){
+    usage( progname );
+    exit(EXIT_FAILURE);
+  }
+  if ( opts.extract('h' ) ){
+    usage( progname );
+    exit(EXIT_SUCCESS);
+  }
+  if ( opts.extract('V' ) ){
+    cerr << PACKAGE_STRING << endl;
+    exit(EXIT_SUCCESS);
+  }
+  int verbosity = 0;
+  while( opts.extract( 'v' ) ){
+    ++verbosity;
+  }
+  string lex_name;
+  opts.extract( "lexicon", lex_name );
+  if ( lex_name.empty() ){
+    cerr << "missing --lexcion options"<< endl;
+    exit(EXIT_FAILURE);
+  }
+  string out_name;
+  opts.extract( 'o', out_name );
+  if ( !opts.empty() ){
+    cerr << "unsupported options : " << opts.toString() << endl;
+    usage(progname);
+    exit(EXIT_FAILURE);
+  }
+  vector<string> fileNames = opts.getMassOpts();
+  if ( fileNames.empty() ){
+    cerr << "missing an inputfile" << endl;
+    exit(EXIT_FAILURE);
+  }
+  if ( fileNames.size() > 1 ){
+    cerr << "only one inputfile may be provided." << endl;
+    exit(EXIT_FAILURE);
+  }
+  string in_name = fileNames[0];
+  if ( !out_name.empty() ){
+    if ( out_name == in_name ){
+      cerr << "same filename for input and output!" << endl;
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  ifstream input( in_name );
+  if ( !input ){
+    cerr << "problem opening input file: " << in_name << endl;
+    exit(1);
+  }
+
+  cout << "results in " << out_name << endl;
+}
