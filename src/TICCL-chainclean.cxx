bd3b363f27b65d84c7da1bf0eeb37db9ce13ed83
Ko van der Sloot <K.vanderSloot@let.ru.nl>
2018-10-08 14:17:54 +0200

    first attempt for chain cleaning

--- src/TICCL-chainclean.cxx
+++ src/TICCL-chainclean.cxx
@@ -56,13 +56,25 @@ void usage( const string& name ){
   cerr << "usage: " << name << "[options] chainfile " << endl;
   cerr << "\t\t The chainfiles is an outputfile from TICCL-chain." << endl;
   cerr << "-t--lexicon A validated lexicon." << endl;
-  cerr << "-t--artifrq The artifreq. Default 100000000." << endl;
+  cerr << "-t--artifrq The artifreq. Default 100000000 ." << endl;
   cerr << "\t-o <outputfile> name of the outputfile." << endl;
   cerr << "\t-h or --help this message." << endl;
   cerr << "\t-v be verbose, repeat to be more verbose. " << endl;
   cerr << "\t-V or --version show version. " << endl;
   exit( EXIT_FAILURE );
 }
+
+const string SEPARATOR = "_";
+
+class record {
+public:
+  string variant;
+  vector<string> v_parts;
+  string v_freq;
+  string cc;
+  string cc_freq;
+  string ld;
+};
 
 int main( int argc, char **argv ){
   TiCC::CL_Options opts;
@@ -92,6 +104,14 @@ int main( int argc, char **argv ){
   int verbosity = 0;
   while( opts.extract( 'v' ) ){
     ++verbosity;
+  }
+  unsigned int artifreq = 100000000;
+  string value;
+  if ( opts.extract( "artifrq", value ) ){
+    if ( !TiCC::stringTo(value,artifreq) ) {
+      cerr << "illegal value for --artifrq (" << value << ")" << endl;
+      exit(EXIT_FAILURE);
+    }
   }
   string lex_name;
   opts.extract( "lexicon", lex_name );
@@ -128,6 +148,103 @@ int main( int argc, char **argv ){
     cerr << "problem opening input file: " << in_name << endl;
     exit(1);
   }
-
+  set<string> valid_words;
+  ifstream lexicon( lex_name );
+  string line;
+  while ( getline( lexicon, line ) ){
+    if ( line.size() == 0 || line[0] == '#' )
+      continue;
+    vector<string> vec = TiCC::split( line );
+    if ( vec.size() < 2 ){
+      cerr << progname << ": invalid line '" << line << "' in " << lex_name << endl;
+      exit( EXIT_FAILURE );
+    }
+    unsigned int freq = 0;
+    if ( !TiCC::stringTo(vec[1], freq) ) {
+      cerr << progname << ": invalid frequency in '" << line << "' in " << lex_name << endl;
+      exit( EXIT_FAILURE );
+    }
+    if ( freq >= artifreq ){
+      valid_words.insert( vec[0] );
+    }
+  }
+  cout << "read " << valid_words.size() << " validated words from "
+       << lex_name << endl;
+  cout << "start reading chained results" << endl;
+  list<record> records;
+  while ( getline( input, line ) ){
+    vector<string> vec = TiCC::split_at( line, "#" );
+    if ( vec.size() != 6 ){
+      cerr << progname << ": chained file should have 6 items per line: '" << line << "' in " << lex_name << endl;
+      cerr << "\t found " << vec.size() << endl;
+      exit( EXIT_FAILURE );
+    }
+    record rec;
+    rec.variant = vec[0];
+    rec.v_freq = vec[1];
+    rec.cc = vec[2];
+    rec.cc_freq = vec[3];
+    rec.ld = vec[4];
+    records.push_back( rec );
+  }
+  cout << "start processing " << records.size() << " chained results" << endl;
+  map<string,int> parts_freq;
+  for ( auto& rec : records ){
+    rec.v_parts = TiCC::split_at( rec.variant, SEPARATOR );
+    if ( rec.v_parts.size() == 1 ){
+      continue;
+    }
+    for ( const auto& p : rec.v_parts ){
+      if ( valid_words.find( p ) == valid_words.end() ){
+	++parts_freq[p];
+      }
+    }
+  }
+  cout << "found " << parts_freq.size() << " unknown parts" << endl;
+  list<record> output_records;
+  for ( const auto& part : parts_freq ) {
+    map<string,int> cc_freqs;
+    auto it = records.begin();
+    while ( it != records.end() ){
+      if ( it->v_parts.size() != 1 ){
+	bool match = false;
+	for ( const auto& p : it->v_parts ){
+	  if ( p == part.first ){
+	    match = true;
+	    break;
+	  }
+	}
+	if ( match ){
+	  ++cc_freqs[it->cc];
+	}
+      }
+      ++it;
+    }
+    cerr << "found " << cc_freqs.size() << " CC's for: " << part.first << endl;
+    cerr << cc_freqs << endl;
+    for ( const auto& cc : cc_freqs ){
+      auto it = records.begin();
+      while ( it != records.end() ){
+	if ( cc.first == it->cc ){
+	  bool match = false;
+	  for ( const auto& p : it->v_parts ){
+	    if ( p == part.first ){
+	      match = true;
+	      break;
+	    }
+	  }
+	  if ( match ){
+	    output_records.push_back( *it );
+	  }
+	}
+	++it;
+      }
+    }
+  }
+  ofstream os( out_name );
+  for ( const auto& it : output_records ){
+    os << it.variant << "#" << it.v_freq << "#" << it.cc << "#"
+       << it.cc_freq << "#" << it.ld << "#C" << endl;
+  }
   cout << "results in " << out_name << endl;
 }
