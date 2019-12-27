63a898fef86168bca2ef2e4bbc1455c0ac21cbb4
Ko van der Sloot <K.vanderSloot@let.ru.nl>
2019-12-27 12:49:34 +0100

    enter 2020. also added some copyright info

--- src/word2vec.cxx
+++ src/word2vec.cxx
@@ -1,3 +1,28 @@
+/*
+  Copyright (c) 2019 - 2020
+  CLST  - Radboud University
+
+  This file is part of ticcltools
+
+  ticcltools is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  ticcltools is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  For questions and suggestions, see:
+      https://github.com/LanguageMachines/ticcltools/issues
+  or send mail to:
+      lamasoftware (at ) science.ru.nl
+
+*/
 #include <iostream>
 #include <cassert>
 #include <cmath>
